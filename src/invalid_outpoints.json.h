a88bc0980f0966e0f4cbbb784b72ab814a1a2f75
Colin Zhang <git@baas.io>
2019-04-20 01:37:06 -0700

    first commit

---
+++ src/invalid_outpoints.json.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2018 The PIVX developers
+// Copyright (c) 2018-2019 The BaaS developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BaaS_INVALID_OUTPOINTS_JSON_H
+#define BaaS_INVALID_OUTPOINTS_JSON_H
+#include <string>
+
+std::string LoadInvalidOutPoints()
+{
+    std::string str = "[]";
+    return str;
+}
+
+#endif //BaaS_INVALID_OUTPOINTS_JSON_H
---
+++ src/invalid_serials.json.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2018 The PIVX developers
+// Copyright (c) 2018-2019 The BaaS developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+
+
+#ifndef BaaS_INVALID_SERIALS_JSON_H
+#define BaaS_INVALID_SERIALS_JSON_H
+
+#include <string>
+
+std::string LoadInvalidSerials()
+{
+    std::string str = "[]";
+    return str;
+}
+
+#endif //BaaS_INVALID_SERIALS_JSON_H
