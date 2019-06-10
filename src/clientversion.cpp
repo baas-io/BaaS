9bf43e1dc0958f7ac61e37476bd651c5c1b0fbe5
WallaceQA <wallace@baas.io>
2019-06-10 23:08:54 +0800

    version update

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -43,10 +43,10 @@ const std::string CLIENT_NAME("BaaS Core");
 #include "obj/build.h"
 #endif
 
-//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives.
-#define GIT_ARCHIVE 1
+//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. $Format:%n#define GIT_ARCHIVE 1$
 #ifdef GIT_ARCHIVE
-#define BUILD_SUFFIX 20190423
+#define GIT_COMMIT_ID "$Format:%H$"
+#define GIT_COMMIT_DATE "$Format:%cD$"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
